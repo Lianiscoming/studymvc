d5a7dfc88d37831c4a849b2f3246cbcd791a1e78
kallis0926 <kallis0926@naver.com>
2023-12-29 15:01:08 +0900

    first commit

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    // 실무에서는 ConcurrentHaskMap을 동시성 이유로 사용해야 함.
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+
+    }
+}
